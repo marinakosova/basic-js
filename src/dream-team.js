6227e8ff47a1881ac2ee28d3b0e051058f100dd0
marinakosova <marina.kosova@gmail.com>
2020-09-30 12:20:00 -0500

    feat: solve dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    let finalString = [];
+
+    if (!Array.isArray(members)) {
+        return false;
+    } else {
+        for (let i = 0; i < members.length; i++) {
+            if (typeof members[i] == "string" && members[i] !== '') {
+                let stringLett = members[i].trim().split('');
+                finalString.push(stringLett[0].toUpperCase());
+            }
+        }
+    }
+
+    return finalString.sort().join('');
+}
