3b13170bd10534417eeb38a0831abe169a0db0b4
marinakosova <marina.kosova@gmail.com>
2020-10-05 11:40:16 -0500

    feat: add hanoi solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let num = solveHanoiTower(disksNumber);
+
+    let result = {
+        turns: num,
+        seconds: Math.floor(num / turnsSpeed * 3600)
+    }
+
+    return result;
 };
+
+function solveHanoiTower(disk) {
+    let counter = 0;
+
+    if (disk > 0) {
+        counter = Math.pow(2, disk) - 1;
+    }
+
+    return counter;
+}
