e1cb30cb90536b7b250cfa8b3a5d79ec7b2c50bf
marinakosova <marina.kosova@gmail.com>
2020-10-10 22:11:33 -0500

    feat: add simple chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    arrayOfStrings: [],
+    getLength() {
+        let len = this.arrayOfStrings.length;
+        this.arrayOfStrings = [];
+        return len;
+    },
+    addLink(value) {
+        if (typeof String('value')) {
+            this.arrayOfStrings.push(`( ${value} )`);
+            return this;
+        } else {
+            this.arrayOfStrings.push('(  )');
+            return this;
+        }
+    },
+    removeLink(position) {
+        if (Number.isInteger(position) && position > 0 && position <= this.arrayOfStrings.length) {
+            this.arrayOfStrings.splice(position - 1, 1);
+            return this;
+        } else {
+            this.arrayOfStrings = [];
+            throw new Error('Error');
+
+        }
+    },
+    reverseChain() {
+        this.arrayOfStrings.reverse();
+        return this;
+    },
+    finishChain() {
+        let finalChain = this.arrayOfStrings.join('~~').toString();
+        this.arrayOfStrings = [];
+        return finalChain;
+    }
 };
 
 module.exports = chainMaker;
