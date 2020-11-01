c085eee7789290d5515f1e9d3e2ccf042aad9028
marinakosova <marina.kosova@gmail.com>
2020-10-31 23:17:41 -0500

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    str = String(str);
+
+    let repeatTimes = options.repeatTimes || 1;
+    let separator = options.separator || '+';
+    let addition = options.addition || '';
+    let additionRepeatTimes = options.additionRepeatTimes || 1;
+    let additionSeparator = options.additionSeparator || '|';
+
+    let arr = [];
+
+    for (let i = 0; i < repeatTimes; i++) {
+        let arrStr = str;
+        let arrStr2 = [];
+        for (let j = 0; j < additionRepeatTimes; j++) {
+            arrStr2.push(String(addition));
+        }
+        arrStr = arrStr + arrStr2.join(additionSeparator);
+        arr.push(arrStr);
+    }
+
+    return arr.join(separator);
+}
